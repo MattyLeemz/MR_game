da47c5b7f895f6d2ae39362576707bb014520b3f
Matt Lima <matt_lima123@hotmail.com>
2020-02-16 16:59:32 -0500

    added movement script from oris and made the camera the player

---
+++ Assets/testMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testMovement : MonoBehaviour
+{
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+    public float moveSpeed = 5f;
+
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraAndMovement();
+    }
+
+    void cameraAndMovement()
+    {
+        //Camera
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+        //movement
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+        //Cursor visibility
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
