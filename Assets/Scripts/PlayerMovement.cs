956b4581d98529cc3f0790e94a0932c84a87015a
Matt Lima <matt_lima123@hotmail.com>
2020-05-09 19:59:45 -0400

    FINALLY fixed movement after 4 painful hours

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,34 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public Rigidbody rigidBody;
-    public bool isRunning = true;
-    Vector3 movement;
+    public CharacterController controller;
 
+    public float speed = 12f;
+    // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.z = Input.GetAxisRaw("Vertical");
-        if (Input.GetKeyDown(KeyCode.LeftShift) && isRunning == false)
-        {
-            isRunning = true;
-        }
-        else
-        {
-            isRunning = false;
-        }
-    }
-    void FixedUpdate()
-    {
-        if (isRunning)
-        {
-            rigidBody.MovePosition(rigidBody.position + (movement * (moveSpeed * 1.5f) * Time.fixedDeltaTime));
-        }
-        else
-        {
-            rigidBody.MovePosition(rigidBody.position + (movement * moveSpeed * Time.fixedDeltaTime));
-        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
     }
 }
+
